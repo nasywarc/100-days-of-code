4e374190fe2ddea37e0f95930c32bea1efa90b46
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-03 19:57:09 +0700

    adding shuffle and modification

--- day-5/day-5-project.py
+++ day-5/day-5-project.py
@@ -11,18 +11,28 @@ total_numbers = int(input("How many numbers would you like?\n"))
 number_list = []
 password_list = []
 
-for i in range(0, 10):
-    number_list.append(i)
+number_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
 symbol_list = ['!', '@', '#', '$', '%', '?', '&', '*']
+letter_cap_list = []
+for i in letter_list:
+    letter_cap_list.append(i.upper())
+
+letter_list += letter_cap_list
+# print(letter_list)
 
 random_letter = (random.sample(letter_list, k=total_letter))
 random_symbol = (random.sample(symbol_list, k=total_symbols))
 random_number = (random.sample(number_list, k=total_numbers))
+random_cap_letter = (random.sample(letter_cap_list, k=total_numbers))
 
 password_list = random_letter + random_symbol + random_number
+random.shuffle(password_list)
 print(password_list)
+
+# ' '.join(password_list)
+# print(' '.join(password_list))
 # print(number_list)
 # print(letter_list)
 # print(symbol_list)
