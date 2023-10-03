3861acd3628913f5f3ecea9c151b049314aa3cdb
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-03 10:46:16 +0700

    endline

--- day-5/day-5-project.py
+++ day-5/day-5-project.py
@@ -1,7 +1,7 @@
 print("Welcome to the PyPassword Generator!")
-total_letter = input("How many letters would you like in your password?")
-total_symbols = input("How many symbols would you like?")
-total_numbers = input("How many numbers would you like?")
+total_letter = input("How many letters would you like in your password?\n")
+total_symbols = input("How many symbols would you like?\n")
+total_numbers = input("How many numbers would you like?\n")
 
 # proccess
 
