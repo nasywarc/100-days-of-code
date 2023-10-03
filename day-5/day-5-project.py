08515c2c98d10d56f7688f589b435b69d17d9e79
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-03 10:54:46 +0700

    change data type to int

--- day-5/day-5-project.py
+++ day-5/day-5-project.py
@@ -1,7 +1,8 @@
 print("Welcome to the PyPassword Generator!")
-total_letter = input("How many letters would you like in your password?\n")
-total_symbols = input("How many symbols would you like?\n")
-total_numbers = input("How many numbers would you like?\n")
+total_letter = int(
+    input("How many letters would you like in your password?\n"))
+total_symbols = int(input("How many symbols would you like?\n"))
+total_numbers = int(input("How many numbers would you like?\n"))
 
 # proccess
 
