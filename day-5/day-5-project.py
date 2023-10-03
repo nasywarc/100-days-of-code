a826d0130c2236f45a6bad82036a2b62aa83067c
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-03 10:44:58 +0700

    adding code to day 5 project, autoformat

---
+++ day-5/day-5-project.py
@@ -0,0 +1,8 @@
+print("Welcome to the PyPassword Generator!")
+total_letter = input("How many letters would you like in your password?")
+total_symbols = input("How many symbols would you like?")
+total_numbers = input("How many numbers would you like?")
+
+# proccess
+
+print("Here is your password: ")
