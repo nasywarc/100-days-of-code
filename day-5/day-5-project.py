d3aebc44c8a955deac2523dd50950b214b74713b
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-03 19:26:03 +0700

    make a randomize list

--- day-5/day-5-project.py
+++ day-5/day-5-project.py
@@ -1,25 +1,30 @@
 import random
 
-# print("Welcome to the PyPassword Generator!")
-# total_letter = int(
-#     input("How many letters would you like in your password?\n"))
-# total_symbols = int(input("How many symbols would you like?\n"))
-# total_numbers = int(input("How many numbers would you like?\n"))
+print("Welcome to the PyPassword Generator!")
+total_letter = int(
+    input("How many letters would you like in your password?\n"))
+total_symbols = int(input("How many symbols would you like?\n"))
+total_numbers = int(input("How many numbers would you like?\n"))
 
 # proccess
 
 number_list = []
+password_list = []
 
 for i in range(0, 10):
     number_list.append(i)
-
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
-
 symbol_list = ['!', '@', '#', '$', '%', '?', '&', '*']
 
-random_number = (random.sample(number_list, k=2))
-print(random_number)
+random_letter = (random.sample(letter_list, k=total_letter))
+random_symbol = (random.sample(symbol_list, k=total_symbols))
+random_number = (random.sample(number_list, k=total_numbers))
+
+password_list.append(random_letter)
+password_list.append(random_symbol)
+password_list.append(random_number)
+print(str(password_list))
 # print(number_list)
 # print(letter_list)
 # print(symbol_list)
