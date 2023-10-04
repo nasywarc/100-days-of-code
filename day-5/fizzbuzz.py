e87725dfc7a62513ef286dfb6e6ae762d6fdde77
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-04 11:58:42 +0700

    fixing debug fizzbuzz

--- day-5/fizzbuzz.py
+++ day-5/fizzbuzz.py
@@ -1,8 +1,9 @@
 # Write your code here 👇
 
 for i in range(1, 101):
-    if i % 3 != 0 or i % 5 != 0:
-        print(i)
+    if i % 3 != 0:
+        if i % 5 != 0:
+            print(i)
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     if i % 3 == 0 and i % 5 != 0:
