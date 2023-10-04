a2739ca27754db354a72f19eefe1a88efb0c2077
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-04 10:53:50 +0700

    adding template from auditorium

---
+++ day-5/high-score.py
@@ -0,0 +1,6 @@
+# Input a list of student scores
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+
+# Write your code below this row 👇
