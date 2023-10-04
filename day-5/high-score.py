654403240c240e41931c14b3f2b6ed27286cf5ae
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-04 10:59:30 +0700

    little modification to the spaces

--- day-5/high-score.py
+++ day-5/high-score.py
@@ -4,9 +4,9 @@ for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 
 # Write your code below this row 👇
+
 max_score = 0
 for i in student_scores:
     if i > max_score:
         max_score = i
-
 print(f"The highest score in the class is: {max_score}")
