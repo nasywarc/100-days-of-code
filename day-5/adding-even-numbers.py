5e757f3526e0bdf5e64c8713d0cf5962d01002c1
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-04 11:19:44 +0700

    finishing the even number code

--- day-5/adding-even-numbers.py
+++ day-5/adding-even-numbers.py
@@ -2,3 +2,9 @@ target = int(input())  # Enter a number between 0 and 1000
 # 🚨 Do not change the code above ☝️
 
 # Write your code here 👇
+
+even_numbers_total = 0
+for i in range(1, (target+1)):
+    if i % 2 == 0:
+        even_numbers_total += i
+print(even_numbers_total)
