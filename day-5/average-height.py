20e48eaf02d64c9553c136f85ea9ff10c2bf722b
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-04 10:20:46 +0700

    adding average height file and template

---
+++ day-5/average-height.py
@@ -0,0 +1,11 @@
+# Input a Python list of student heights
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
+
+print(f"total height ")
+print(f"number of students ")
+print(f"average height ")
