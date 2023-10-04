de58a0bb83c16841eca66e319e49ac217cf0e350
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-04 18:57:21 +0700

    adding codes

---
+++ day-6/reeborg-hurdle-3.py
@@ -0,0 +1,24 @@
+# def turn_right() :
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump() :
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# def walk_jump() :
+#     move()
+#     jump()
+#     turn_left()
+
+# while not at_goal() :
+#     if front_is_clear() :
+#         move()
+#     else :
+#         jump()
