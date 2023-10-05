171132608eddcb4849976f85e0207cabef6117d4
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-05 19:00:45 +0700

    adding hurdle 4 code

--- day-6/reeborg-hurdle-challange.py
+++ day-6/reeborg-hurdle-challange.py
@@ -1,3 +1,5 @@
+# hurdle challange 3
+
 # def turn_right() :
 #     turn_left()
 #     turn_left()
@@ -17,6 +19,38 @@
 #     jump()
 #     turn_left()
 
+# while not at_goal() :
+#     if front_is_clear() :
+#         move()
+#     else :
+#         jump()
+
+# ============================================
+
+# hurdle challange 4
+
+# def turn_right() :
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump() :
+#     turn_left()
+#     move()
+#     global i
+#     i+=1
+#     turn_right()
+#     if front_is_clear():
+#         move()
+#         turn_right()
+#         for i in range (0, i):
+#             move()
+#         i = 0
+#         turn_left()
+#     else :
+#         jump()
+
+# i = 0
 # while not at_goal() :
 #     if front_is_clear() :
 #         move()
