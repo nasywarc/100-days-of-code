78b8447376c474b247908315917abc4bb96582bf
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-07 13:53:14 +0700

    adding correct code

---
+++ day-6/reeborg-intermediate-challange.py
@@ -0,0 +1,32 @@
+# debugging challange infinite loop python course
+# can used to pass the problem, problem2, problem3 file
+
+# def turn_right() :
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def turn_around() :
+#     turn_left()
+#     turn_left()
+
+# def search() :
+#     while not at_goal() :
+#         while front_is_clear() or right_is_clear():
+#             if right_is_clear() and not at_goal() and i < 4:
+#                 turn_right()
+#                 move()
+#                 global i
+#                 i+=1
+
+#             else :
+#                 if not at_goal() :
+#                     move()
+#                     i = 0
+#                 else :
+#                     break
+#                     i = 0
+#         turn_around()
+# i = 0
+# while not at_goal() :
+#     search()
