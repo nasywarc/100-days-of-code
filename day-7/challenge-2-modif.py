d6328776ea30cd8d5764718fe5faec091471cac5
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-08 09:01:00 +0700

    adding codes

--- day-7/challenge-2-modif.py
+++ day-7/challenge-2-modif.py
@@ -2,7 +2,7 @@
 
 import random
 word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = list(random.choice(word_list))
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -13,24 +13,39 @@ print(f'Pssst, the solution is {chosen_word}.')
 display = []
 for letter in chosen_word:
     display.append("_")
-guess = input("Guess a letter : ").lower()
+# guess = input("Guess a letter : ").lower()
 
 # TODO-2: - Loop through each position in the chosen_word;
 # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 # e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-for position in range(len(chosen_word)):
-    if guess == chosen_word[position]:
-        display[position] = (guess)
+# for position in range(len(chosen_word)):
+#     if guess == chosen_word[position]:
+#         display[position] = (guess)
     # else:
     #     display[position] = ["_"]
 
 # TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 # Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+# print(display)
 
-guess = input("Guess a letter : ").lower()
-for position in range(len(chosen_word)):
-    if guess == chosen_word[position]:
-        display[position] = (guess)
-print(display)
+life = 11
+while display != chosen_word and life > 0:
+    guess = input("Guess a letter : ").lower()
+    if guess in chosen_word:
+        for position in range(len(chosen_word)):
+            if guess == chosen_word[position]:
+                display[position] = (guess)
+    else:
+        life -= 1
+        print(f"Your life is : {life}")
+
+    print(display)
+
+if life > 0:
+    print("\nYou WIN!")
+else:
+    print("\nGame Over.")
+
+# print(type(display))
+# print(type(chosen_word))
