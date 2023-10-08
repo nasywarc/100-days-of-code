a18253de08e250c4fd2ac5e0eb2ba4c53c48f931
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-08 15:58:19 +0700

    finding bugs and fix it

--- day-7/challenge-4-modif.py
+++ day-7/challenge-4-modif.py
@@ -1,7 +1,6 @@
 # Step 2 mix 3 mix 4
 
 import random
-# word_list = ["aardvark", "baboon", "camel"]
 word_list = ["kucing", "anjing", "semut", "singa", "harimau",
              "hamster", "burung", "tikus", "gajah", "jerapah",
              "angsa", "ikan", "rusa", "macan", "ular", "siput",
@@ -10,7 +9,7 @@ word_list = ["kucing", "anjing", "semut", "singa", "harimau",
              "bebek", "itik", "rubah", "monyet", "babi", "orangutan"]
 
 chosen_word = list(random.choice(word_list))
-# print(f'Pssst, the solution is {chosen_word}.')
+print(chosen_word)
 
 stages = ['''
   +---+
@@ -81,6 +80,7 @@ print(
                    |___/                       
 '''
 )
+
 display = []
 for letter in chosen_word:
     display.append("_")
@@ -91,29 +91,23 @@ guess_list = []
 while display != chosen_word and life > 0:
     guess = input("\nGuess a letter : ").lower()
     if guess in guess_list:
-        life -= 1
         print("\nYou've already guess that letter.")
-        print(f"Your life has decreased to {life}\n")
 
     elif guess in chosen_word:
         for position in range(len(chosen_word)):
             if guess == chosen_word[position]:
                 display[position] = (guess)
                 guess_list.append(guess)
-                print(f"{' '.join(display)}")
+        print(f"{' '.join(display)}")
 
     else:
         life -= 1
-        # print("\nWrong.")
-        # print(f"Your life has decreased to {life}\n")
         print(f"{' '.join(display)}")
         print(stages[life])
 
-    # print(f"{' '.join(display)}")
 
 if life > 0:
     print("\nCongrats, You WIN!")
 else:
-    # print(f"\nThe answer is {chosen_word}")
     print(f"The answer is \"{' '.join(chosen_word)}\"")
     print("\nYou lose, Game Over.")
