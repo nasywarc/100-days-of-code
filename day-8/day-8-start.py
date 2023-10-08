aa21f5e8ebc9346a56bf366624f382f6e95d5ce8
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-09 06:41:36 +0700

    adding codes

--- day-8/day-8-start.py
+++ day-8/day-8-start.py
@@ -2,3 +2,11 @@
 # Create a function called greet().
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
+
+def greet():
+    print("This")
+    print("Is")
+    print("Function")
+
+
+greet()
