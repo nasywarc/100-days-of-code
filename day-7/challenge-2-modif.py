38cdc6c66166a8f2b7ec01d63c3f9cd6a933e1f8
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-08 09:09:24 +0700

    fixing bug 2

--- day-7/challenge-2-modif.py
+++ day-7/challenge-2-modif.py
@@ -14,16 +14,16 @@ guess_list = []
 
 while display != chosen_word and life > 0:
     guess = input("Guess a letter : ").lower()
-    guess_list.append(guess)
     if guess in guess_list:
         life -= 1
-        print("You've already guess that letter.")
-        print(f"Your life has decreased to {life}")
+        print("\nYou've already guess that letter.")
+        print(f"Your life has decreased to {life}\n")
 
     elif guess in chosen_word:
         for position in range(len(chosen_word)):
             if guess == chosen_word[position]:
                 display[position] = (guess)
+                guess_list.append(guess)
     else:
         life -= 1
         print(f"Your life is : {life}")
