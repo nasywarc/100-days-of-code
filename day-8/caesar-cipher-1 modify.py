8c343c4c110ae57c4ba8975a4d34e884aef22c41
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-09 08:11:06 +0700

    continue the code

--- day-8/caesar-cipher-1 modify.py
+++ day-8/caesar-cipher-1 modify.py
@@ -8,11 +8,17 @@ shift = int(input("Type the shift number:\n"))
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
 
-def encrypt(text, shift):
-    plain_text = text
-    number_of_shift = shift
-    for letter in text:
-        letter += shift
+def encrypt(plain_text, shift_amount):
+    index_list = []
+    new_word = []
+    for letter in plain_text:
+        index_list.append(alphabet.index(letter))
+    for index in range(len(index_list)):
+        index_list[index] += shift_amount
+    for cipher in index_list:
+        new_word.append((alphabet[cipher]))
+    print("".join(new_word))
+
 # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
 # e.g.
 # plain_text = "hello"
@@ -25,4 +31,6 @@ def encrypt(text, shift):
 
 # 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
+
 # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+encrypt(text, shift)
