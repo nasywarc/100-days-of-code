408e5bc3da9dd87848ff8ddef229ff8e33614119
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-09 07:08:34 +0700

    adding function

--- day-8/paint-area-calculator.py
+++ day-8/paint-area-calculator.py
@@ -1,8 +1,14 @@
 # Write your code below this line 👇
+import math
 
 
+def paint_calc(height, width, cover):
+    # func to rounded up number
+    number_of_cans = math.ceil((height*width)/coverage)
+    print(f"You'll need {number_of_cans} cans of paint.")
 # Write your code above this line 👆
 # Define a function called paint_calc() so the code below works.
+
 
 # 🚨 Don't change the code below 👇
 test_h = int(input())  # Height of wall (m)
