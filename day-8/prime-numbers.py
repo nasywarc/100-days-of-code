af2fcb1789fa690756e3c096675f09b812396307
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-09 07:32:45 +0700

    adding function

--- day-8/prime-numbers.py
+++ day-8/prime-numbers.py
@@ -1,7 +1,15 @@
 # Write your code below this line 👇
-
-
+def prime_checker(number):
+    divider = 0
+    for i in range(47):
+        if number % (i+1) == 0:
+            divider += 1
+    if divider <= 2:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
 # Write your code above this line 👆
+
 
 # Do NOT change any of the code below👇
 n = int(input())  # Check this number
