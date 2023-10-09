fe9b99acbecbc618da4e00fef22b33052101ebd5
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-09 08:23:29 +0700

    if else encode decode

--- day-8/caesar-cipher-1 modify.py
+++ day-8/caesar-cipher-1 modify.py
@@ -2,8 +2,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
@@ -19,7 +17,20 @@ def encrypt(plain_text, shift_amount):
         new_word.append((alphabet[cipher]))
     print("".join(new_word))
 
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+
+def decrypt(cipher_text, shift_amount):
+    index_list = []
+    new_word = []
+    for letter in cipher_text:
+        index_list.append(alphabet.index(letter))
+    for index in range(len(index_list)):
+        index_list[index] -= shift_amount
+    for cipher in index_list:
+        new_word.append((alphabet[cipher]))
+    print("".join(new_word))
+
+# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards
+# in the alphabet by the shift amount and print the encrypted text.
 # e.g.
 # plain_text = "hello"
 # shift = 5
@@ -33,4 +44,13 @@ def encrypt(plain_text, shift_amount):
 
 
 # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-encrypt(text, shift)
+if direction == 'encode':
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    encrypt(text, shift)
+elif direction == 'decode':
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    decrypt(text, shift)
+else:
+    print("Your input is invalid.")
