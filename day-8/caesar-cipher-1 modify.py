ad318062170271540dc5b6eec6d959ef47e23250
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-09 08:27:03 +0700

    deleting some comments

--- day-8/caesar-cipher-1 modify.py
+++ day-8/caesar-cipher-1 modify.py
@@ -30,12 +30,6 @@ def decrypt(cipher_text, shift_amount):
     print("".join(new_word))
 
 # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards
-# in the alphabet by the shift amount and print the encrypted text.
-# e.g.
-# plain_text = "hello"
-# shift = 5
-# cipher_text = "mjqqt"
-# print output: "The encoded text is mjqqt"
 
 # HINT: How do you get the index of an item in a list:
 # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
