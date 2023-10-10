4ac44f74d60dd25c1eea96379cd0badae51fbeec
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-10 10:29:59 +0700

    completing caesar func

--- day-8/caesar-cipher-3-modify.py
+++ day-8/caesar-cipher-3-modify.py
@@ -3,31 +3,27 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-def caesar (text, shift, direction) :
+
+def caesar(text, shift, direction):
     index_list = []
     new_word = []
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
     for letter in text:
         index_list.append(alphabet.index(letter))
     for index in range(len(index_list)):
-        if direction == 'encode' :
+        if direction == 'encode':
             index_list[index] += shift
             while index_list[index] > 25:
                 index_list[index] -= 26
-        elif direction == 'decode' :
-            
+        elif direction == 'decode':
+            while index_list[index] < 0:
+                index_list[index] += 26
+        else:
+            print("Your input is invalid.")
     for cipher in index_list:
         new_word.append((alphabet[cipher]))
 
-    if direction == 'encode':
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        encrypt(text, shift)
-    elif direction == 'decode':
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        decrypt(text, shift)
-    else:
-        print("Your input is invalid.")
 
 def encrypt(plain_text, shift_amount):
     index_list = []
