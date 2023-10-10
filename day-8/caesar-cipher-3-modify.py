bb8ad6e3e28c3254c7900b47e604878c7dbaf263
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-10 10:28:27 +0700

    combine encode and decode

--- day-8/caesar-cipher-3-modify.py
+++ day-8/caesar-cipher-3-modify.py
@@ -3,15 +3,37 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
+def caesar (text, shift, direction) :
+    index_list = []
+    new_word = []
+    for letter in text:
+        index_list.append(alphabet.index(letter))
+    for index in range(len(index_list)):
+        if direction == 'encode' :
+            index_list[index] += shift
+            while index_list[index] > 25:
+                index_list[index] -= 26
+        elif direction == 'decode' :
+            
+    for cipher in index_list:
+        new_word.append((alphabet[cipher]))
+
+    if direction == 'encode':
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        encrypt(text, shift)
+    elif direction == 'decode':
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        decrypt(text, shift)
+    else:
+        print("Your input is invalid.")
 
 def encrypt(plain_text, shift_amount):
     index_list = []
     new_word = []
     for letter in plain_text:
-        if letter in alphabet:
-            index_list.append(alphabet.index(letter))
-        # else:
-            # index_list.append(int(" "))                   # bug
+        index_list.append(alphabet.index(letter))
     for index in range(len(index_list)):
         index_list[index] += shift_amount
         while index_list[index] > 25:
