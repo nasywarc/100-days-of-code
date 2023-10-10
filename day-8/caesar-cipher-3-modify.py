7ac886564b0e00f3960b425c7fed916115ac14dc
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-10 10:21:46 +0700

    change the code

--- day-8/caesar-cipher-3-modify.py
+++ day-8/caesar-cipher-3-modify.py
@@ -1,34 +1,47 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-# TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
 
 
 def encrypt(plain_text, shift_amount):
-    cipher_text = ""
+    index_list = []
+    new_word = []
     for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        cipher_text += alphabet[new_position]
-    print(f"The encoded text is {cipher_text}")
+        if letter in alphabet:
+            index_list.append(alphabet.index(letter))
+        # else:
+            # index_list.append(int(" "))                   # bug
+    for index in range(len(index_list)):
+        index_list[index] += shift_amount
+        while index_list[index] > 25:
+            index_list[index] -= 26
+    for cipher in index_list:
+        new_word.append((alphabet[cipher]))
+    print("".join(new_word))
 
 
 def decrypt(cipher_text, shift_amount):
-    plain_text = ""
+    index_list = []
+    new_word = []
     for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        plain_text += alphabet[new_position]
-    print(f"The decoded text is {plain_text}")
+        index_list.append(alphabet.index(letter))
+    for index in range(len(index_list)):
+        index_list[index] -= shift_amount
+        while index_list[index] < 0:
+            index_list[index] += 26
+    for cipher in index_list:
+        new_word.append((alphabet[cipher]))
+    print("".join(new_word))
 
 
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
-
-# TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+if direction == 'encode':
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    encrypt(text, shift)
+elif direction == 'decode':
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    decrypt(text, shift)
+else:
+    print("Your input is invalid.")
