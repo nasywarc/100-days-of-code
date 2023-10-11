fc29b00771686ad0c22ee98092df43313ca1e2ca
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-12 06:26:31 +0700

    rearrange some code

--- day-8/caesar-cipher-3-modify.py
+++ day-8/caesar-cipher-3-modify.py
@@ -1,10 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 
 def caesar(text, shift, direction):
     index_list = []
@@ -31,5 +27,9 @@ def caesar(text, shift, direction):
     else:
         print("\nYour input is invalid.")
 
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 caesar(text=text, direction=direction, shift=shift)
