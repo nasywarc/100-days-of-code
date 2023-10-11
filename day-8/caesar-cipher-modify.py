fe3336618a2e9213b6385216f3f7fb691aa990d3
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-12 06:39:55 +0700

    continue func

--- day-8/caesar-cipher-modify.py
+++ day-8/caesar-cipher-modify.py
@@ -2,7 +2,14 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def caesar(text, shift, direction):
-    letter_index = []
+def caesar(start_word, shift, direction):
+    letter = []
     symbol = []
-    new_word = []
+    letter_index = []
+    symbol_index = []
+    end_word = []
+
+    for pos in range(len(start_word)):
+        if start_word[pos] in alphabet:
+            letter_index.append(pos)
+            letter.append(alphabet.index(start_word[pos]))
