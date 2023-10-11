77bd57763f5d55e666c7f87575dcdeb9c21946bd
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-11 11:05:24 +0700

    testing

--- caesar-cipher-4-modify.py
+++ caesar-cipher-4-modify.py
@@ -17,30 +17,32 @@ def caesar(text, shift, direction):
         else:
             symbol.append(letter)
             symbol_index.append(symbol.index(letter))
-    for index in range(len(index_list)):
-        if direction == 'encode':
-            if index in index_list:
-                index_list[index] += shift
-            while index_list[index] > 25:
-                index_list[index] -= 26
-        elif direction == 'decode':
-            if index in index_list:
-                index_list[index] -= shift
-            while index_list[index] < 0:
-                index_list[index] += 26
-        else:
-            break
-    for cipher in index_list:
-        if cipher in alphabet:
-            new_word.append((alphabet[cipher]))
-        else:
-            new_word.append((symbol_index[cipher]))
-    if direction == 'encode':
-        print("The encoded text is", "".join(new_word))
-    elif direction == 'decode':
-        print("The decoded text is", "".join(new_word))
-    else:
-        print("\nYour input is invalid.")
+    # for join in range(len(text)):
+    #     if direction == 'encode':
+    #         for index in text :
+
+        #     if join in index_list:
+        #         index_list[index] += shift
+        #     while index_list[index] > 25:
+        #         index_list[index] -= 26
+        # elif direction == 'decode':
+        #     if index in index_list:
+        #         index_list[index] -= shift
+        #     while index_list[index] < 0:
+        #         index_list[index] += 26
+    #     else:
+    #         break
+    # for cipher in index_list:
+    #     if cipher in alphabet:
+    #         new_word.append((alphabet[cipher]))
+    #     else:
+    #         new_word.append((symbol_index[cipher]))
+    # if direction == 'encode':
+    #     print("The encoded text is", "".join(new_word))
+    # elif direction == 'decode':
+    #     print("The decoded text is", "".join(new_word))
+    # else:
+    #     print("\nYour input is invalid.")
 
 
-caesar(text=text, direction=direction, shift=shift)
+# caesar(text=text, direction=direction, shift=shift)
