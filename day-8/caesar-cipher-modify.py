575813bc3beb0fc3ba926725e7a4de3a7b9d5742
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-13 15:23:40 +0700

    modify codes

--- day-8/caesar-cipher-modify.py
+++ day-8/caesar-cipher-modify.py
@@ -13,40 +13,51 @@ def caesar(start_word, shift, direction):
     new_word = []
     end_word = []
     final_result = ""
-    loop = True
+    for pos in range(len(start_word)):
+        if start_word[pos] in alphabet:
+            letter_index.append(pos)
+            letter.append(alphabet.index(start_word[pos]))
+        else:
+            symbol.append(start_word[pos])
 
-    while loop:
-        for pos in range(len(start_word)):
-            if start_word[pos] in alphabet:
-                letter_index.append(pos)
-                letter.append(alphabet.index(start_word[pos]))
-            else:
-                symbol.append(start_word[pos])
+    if direction == 'decode':
+        shift *= -1
 
-        if direction == 'decode':
-            shift *= -1
+    for index in letter:
+        new_index = index + shift
+        while new_index > 25:
+            new_index -= 26
+        while new_index < 0:
+            new_index += 26
+        new_word.append(alphabet[new_index])
 
-        for index in letter:
-            new_index = index + shift
-            while new_index > 25:
-                new_index -= 26
-            while new_index < 0:
-                new_index += 26
-            new_word.append(alphabet[new_index])
+    start_let = 0
+    start_sym = 0
 
-        start_let = 0
-        start_sym = 0
+    for join in range(len(start_word)):
+        if join in letter_index:
+            end_word.append(new_word[start_let])
+            start_let += 1
+        else:
+            end_word.append(symbol[start_sym])
+            start_sym += 1
 
-        for join in range(len(start_word)):
-            if join in letter_index:
-                end_word.append(new_word[start_let])
-                start_let += 1
-            else:
-                end_word.append(symbol[start_sym])
-                start_sym += 1
+    final_result = "".join(end_word)
+    print(f"Here's the {direction}d result : {final_result}")
 
-        final_result = "".join(end_word)
-        print(f"Here's the {direction}d result : {final_result}")
+
+os.system('cls')
+print(logo)
+
+loop = True
+
+while loop:
+    direction = input(
+        "Type \"encode\" to encrypt, type \"decode\" to decrypt:\n")
+    if direction == 'encode' or direction == 'decode':
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        caesar(start_word=text, direction=direction, shift=shift)
 
         loop_or_no = input(
             "Type \"Yes\" if you want to go again. Otherwise type \"No\"\n")
@@ -60,14 +71,5 @@ def caesar(start_word, shift, direction):
             loop = False
             print("Your input is invalid.")
 
-
-os.system('cls')
-print(logo)
-
-direction = input("Type \"encode\" to encrypt, type \"decode\" to decrypt:\n")
-if direction == 'encode' or direction == 'decode':
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    caesar(start_word=text, direction=direction, shift=shift)
-else:
-    print("Your input is invalid.")
+    else:
+        print("Your input is invalid.")
