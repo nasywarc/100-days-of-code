d5c664f8f867af583fae6e1b95095015ba41afd0
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-13 15:09:55 +0700

    make program loop

--- day-8/caesar-cipher-modify.py
+++ day-8/caesar-cipher-modify.py
@@ -12,37 +12,41 @@ def caesar(start_word, shift, direction):
     new_index = ""
     new_word = []
     end_word = []
+    final_result = ""
+    loop = True
 
-    for pos in range(len(start_word)):
-        if start_word[pos] in alphabet:
-            letter_index.append(pos)
-            letter.append(alphabet.index(start_word[pos]))
-        else:
-            symbol.append(start_word[pos])
+    while loop:
+        for pos in range(len(start_word)):
+            if start_word[pos] in alphabet:
+                letter_index.append(pos)
+                letter.append(alphabet.index(start_word[pos]))
+            else:
+                symbol.append(start_word[pos])
 
-    if direction == 'decode':
-        shift *= -1
+        if direction == 'decode':
+            shift *= -1
 
-    for index in letter:
-        new_index = index + shift
-        while new_index > 25:
-            new_index -= 26
-        while new_index < 0:
-            new_index += 26
-        new_word.append(alphabet[new_index])
+        for index in letter:
+            new_index = index + shift
+            while new_index > 25:
+                new_index -= 26
+            while new_index < 0:
+                new_index += 26
+            new_word.append(alphabet[new_index])
 
-    start_let = 0
-    start_sym = 0
+        start_let = 0
+        start_sym = 0
 
-    for join in range(len(start_word)):
-        if join in letter_index:
-            end_word.append(new_word[start_let])
-            start_let += 1
-        else:
-            end_word.append(symbol[start_sym])
-            start_sym += 1
+        for join in range(len(start_word)):
+            if join in letter_index:
+                end_word.append(new_word[start_let])
+                start_let += 1
+            else:
+                end_word.append(symbol[start_sym])
+                start_sym += 1
 
-    print("".join(end_word))
+        final_result = "".join(end_word)
+        print(f"Here's the {direction}d result : {final_result}")
 
 
 os.system('cls')
