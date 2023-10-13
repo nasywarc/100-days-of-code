3b55eaa126c3120db3d5420f6042e64627d0eaf3
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-13 15:10:48 +0700

    if else statement

--- day-8/caesar-cipher-modify.py
+++ day-8/caesar-cipher-modify.py
@@ -56,4 +56,5 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-caesar(start_word=text, direction=direction, shift=shift)
+if direction == 'encode' or direction == 'decode':
+    caesar(start_word=text, direction=direction, shift=shift)
