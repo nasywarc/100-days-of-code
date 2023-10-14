8dabd41ffa19dfed5c2065aca5332ab1fef8861a
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-14 16:42:07 +0700

    if else statement

--- day-10/calculator.py
+++ day-10/calculator.py
@@ -7,17 +7,35 @@ def add(first, sec):
     return result
 
 
+def sub(first, sec):
+    result = first - sec
+    return result
+
+
+def mul(first, sec):
+    result = first * sec
+    return result
+
+
+def div(first, sec):
+    result = first / sec
+    return result
+
+
 os.system('cls')
 print(art.logo)
 
 first_num = int(input("What's the first number?\n"))
 operation = input("Pick an operation. (\" + \", \" - \", \" * \", \" / \")\n")
-second_num = int(input("What's the next number?\n"))
-if operation == "+":
-    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
-elif operation == "-":
-    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
-elif operation == "*":
-    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
-elif operation == "/":
-    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
+if operation != "+" or operation != "-" or operation != "*" or operation != "/":
+    print("Your input is invalid.")
+else:
+    second_num = int(input("What's the next number?\n"))
+    if operation == "+":
+        print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
+    elif operation == "-":
+        print(f"{first_num} {operation} {second_num} = {sub(first_num, second_num)}")
+    elif operation == "*":
+        print(f"{first_num} {operation} {second_num} = {mul(first_num, second_num)}")
+    elif operation == "/":
+        print(f"{first_num} {operation} {second_num} = {div(first_num, second_num)}")
