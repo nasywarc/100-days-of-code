8095493756d4d74d34e7f9c30fd264029aac0877
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-14 16:38:49 +0700

    adding structure

--- day-10/calculator.py
+++ day-10/calculator.py
@@ -1,8 +1,23 @@
 import art
 import os
 
+
+def add(first, sec):
+    result = first + sec
+    return result
+
+
 os.system('cls')
 print(art.logo)
 
-first_number = input("what's the first number?\n")
-operation = input("Pick an operation. (\"+\", \"-\", \"*\", \"/\")\n")
+first_num = int(input("What's the first number?\n"))
+operation = input("Pick an operation. (\" + \", \" - \", \" * \", \" / \")\n")
+second_num = int(input("What's the next number?\n"))
+if operation == "+":
+    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
+elif operation == "-":
+    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
+elif operation == "*":
+    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
+elif operation == "/":
+    print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
