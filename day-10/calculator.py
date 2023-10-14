b719c9cab010808fd2d31b1fac7f5dd401db3f73
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-14 17:13:49 +0700

    adding codes

--- day-10/calculator.py
+++ day-10/calculator.py
@@ -25,9 +25,9 @@ def div(first, sec):
 os.system('cls')
 print(art.logo)
 
-number = []
+number = [0, 0]
 loop = True
-i = 1
+i = 0
 number[0] = int(input("What's the first number?\n"))
 operation = input("Pick an operation. (\" + \", \" - \", \" * \", \" / \")\n")
 
@@ -35,19 +35,22 @@ if operation != "+" and operation != "-" and operation != "*" and operation != "
     print("Your input is invalid.")
 else:
     while loop:
-        number[i] = int(input("What's the next number?\n"))
+        if i > 0:
+            operation = input(
+                "\nPick an operation. (\" + \", \" - \", \" * \", \" / \")\n")
+        number[1] = int(input("What's the next number?\n"))
         if operation == "+":
-            result = add(number[i-1], number[i])
+            result = add(number[0], number[1])
         elif operation == "-":
-            result = sub(number[i-1], number[i])
+            result = sub(number[0], number[1])
         elif operation == "*":
-            result = mul(number[i-1], number[i])
+            result = mul(number[0], number[1])
         else:
-            result = div(number[i-1], number[i])
-        print(f"\n{number[i-1]} {operation} {number[i]} = {result}")
-        keep_loop = print(
-            f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation.\nInput : ")
+            result = div(number[0], number[1])
+        print(f"\n{number[0]} {operation} {number[1]} = {result}")
+        keep_loop = input(
+            f"\nType 'y' to continue calculating with {result}, or type 'n' to start a new calculation.\nInput : ")
+        number[0] = result
         i += 1
-        number[i-1] = result
         if keep_loop == 'n':
             loop = False
