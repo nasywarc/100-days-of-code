fa4cdb90599a169bb972e6353a47423f5bed8ccb
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-14 16:43:50 +0700

    endline

--- day-10/calculator.py
+++ day-10/calculator.py
@@ -32,10 +32,14 @@ if operation != "+" and operation != "-" and operation != "*" and operation != "
 else:
     second_num = int(input("What's the next number?\n"))
     if operation == "+":
-        print(f"{first_num} {operation} {second_num} = {add(first_num, second_num)}")
+        print(
+            f"\n{first_num} {operation} {second_num} = {add(first_num, second_num)}")
     elif operation == "-":
-        print(f"{first_num} {operation} {second_num} = {sub(first_num, second_num)}")
+        print(
+            f"\n{first_num} {operation} {second_num} = {sub(first_num, second_num)}")
     elif operation == "*":
-        print(f"{first_num} {operation} {second_num} = {mul(first_num, second_num)}")
+        print(
+            f"\n{first_num} {operation} {second_num} = {mul(first_num, second_num)}")
     elif operation == "/":
-        print(f"{first_num} {operation} {second_num} = {div(first_num, second_num)}")
+        print(
+            f"\n{first_num} {operation} {second_num} = {div(first_num, second_num)}")
