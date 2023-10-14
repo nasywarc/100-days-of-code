6bd3cfc6c1d4838cd2cc5aa5f682435948efb2c5
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-14 12:37:18 +0700

    finished program

--- day-10/days_in_month.py
+++ day-10/days_in_month.py
@@ -19,6 +19,10 @@ def is_leap(year):
 
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    is_leap(year)
+    if is_leap == True:
+        month_days[1] = 29
+    return month_days[month-1]
 
 
 # 🚨 Do NOT change any of the code below
