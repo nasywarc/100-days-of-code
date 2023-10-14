2ca898e1bfc664fa340085cb0b33da9f0e4070ac
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-14 11:53:45 +0700

    return func

--- day-10/days_in_month.py
+++ day-10/days_in_month.py
@@ -2,18 +2,22 @@ def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year")
+                # print("Leap year")
+                return True
             else:
-                print("Not leap year")
+                # print("Not leap year")
+                return False
         else:
-            print("Leap year")
+            # print("Leap year")
+            return True
     else:
-        print("Not leap year")
+        # print("Not leap year")
+        return False
 
 # TODO: Add more code here 👇
 
 
-def days_in_month():
+def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
