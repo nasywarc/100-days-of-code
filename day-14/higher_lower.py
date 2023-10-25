ef8fd44dac698b9cfbe1c2e8ac9d2171a90aa71d
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-25 10:08:31 +0700

    finishing the game logic

--- day-14/higher_lower.py
+++ day-14/higher_lower.py
@@ -9,38 +9,58 @@ def pick_the_artist():
     return index_pick
 
 
-os.system('cls')
-print(art.logo)
+def right():
+    return score + 1
+
+
+def wrong():
+    pass
+
+
+def header():
+    os.system('cls')
+    print(art.logo)
+
+
+header()
 
 first_artist = pick_the_artist()
 second_artist = pick_the_artist()
+play = True
 score = 0
 
-print(
-    f'Compare A: {data[first_artist]["artist"]}, a {data[first_artist]["occupation"]}, from {data[first_artist]["born"]}.')
-print(art.vs)
-print(
-    f'Against B: {data[second_artist]["artist"]}, a {data[second_artist]["occupation"]}, from {data[second_artist]["born"]}.')
+while play:
+    print(
+        f'Compare A: {data[first_artist]["artist"]}, a {data[first_artist]["occupation"]}, from {data[first_artist]["born"]}.')
+    print(art.vs)
+    print(
+        f'Against B: {data[second_artist]["artist"]}, a {data[second_artist]["occupation"]}, from {data[second_artist]["born"]}.')
 
-user_answer = input(
-    '\nWho has more followers? Type \'A\' or \'B\'.\nInput -> ').lower()
-if user_answer != 'a' and user_answer != 'b':
-    print('Your input is invalid.')
-    exit()
+    user_answer = input(
+        '\nWho has more followers? Type \'A\' or \'B\'.\nInput -> ').lower()
+    if user_answer != 'a' and user_answer != 'b':
+        print('Your input is invalid.')
+        exit()
 
-if data[first_artist]['followers'] > data[second_artist]['followers']:
-    if user_answer == 'a':
-        score += 1
-        print(f'You\'re right! Current score: {score}.')
+    if data[first_artist]['followers'] > data[second_artist]['followers']:
+        if user_answer == 'a':
+            header()
+            score += 1
+            print(f'You\'re right! Current score: {score}.')
+            first_artist = second_artist
+            second_artist = pick_the_artist()
+        else:
+            header()
+            print(f'Sorry, that\'s wrong. Final score: {score}.\n')
+            play = False
     else:
-        os.system('cls')
-        print(art.logo)
-        print(f'Sorry, that\'s wrong. Final score: {score}.\n')
-else:
-    if user_answer == 'b':
-        score += 1
-        print(f'You\'re right! Current score: {score}.')
-    else:
-        os.system('cls')
-        print(art.logo)
-        print(f'Sorry, that\'s wrong. Final score: {score}.\n')
+        if user_answer == 'b':
+            header()
+            score += 1
+            print(f'You\'re right! Current score: {score}.')
+            first_artist = second_artist
+            second_artist = pick_the_artist()
+        else:
+            header()
+            print(f'Sorry, that\'s wrong. Final score: {score}.\n')
+            play = False
