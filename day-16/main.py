ea4bbbc1d0adc57074863d677a807d6f4cb31826
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-25 16:33:58 +0700

    creating new file

---
+++ day-16/main.py
@@ -0,0 +1,32 @@
+# import another_module
+# print(another_module.another_variable)
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.color('black', 'green4')
+# timmy.shape('turtle')
+
+# timmy.penup()
+# timmy.forward(100)
+# timmy.pendown()
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(200)
+# timmy.left(90)
+# timmy.forward(200)
+# timmy.left(90)
+# timmy.forward(200)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.penup()
+# timmy.home()
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+import prettytable
