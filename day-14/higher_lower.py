d4b607fee8b81e1cb20650c985c748f0c1e4b98a
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-25 09:57:05 +0700

    making condition

--- day-14/higher_lower.py
+++ day-14/higher_lower.py
@@ -6,8 +6,6 @@ import random
 
 def pick_the_artist():
     index_pick = random.randint(0, 24)
-    # artist_choice = data[index_pick]
-    # return artist_choice
     return index_pick
 
 
@@ -16,13 +14,33 @@ print(art.logo)
 
 first_artist = pick_the_artist()
 second_artist = pick_the_artist()
+score = 0
+
 print(
     f'Compare A: {data[first_artist]["artist"]}, a {data[first_artist]["occupation"]}, from {data[first_artist]["born"]}.')
 print(art.vs)
 print(
     f'Against B: {data[second_artist]["artist"]}, a {data[second_artist]["occupation"]}, from {data[second_artist]["born"]}.')
 
-input('\nWho has more followers? Type \'A\' or \'B\'.\nInput -> ')
+user_answer = input(
+    '\nWho has more followers? Type \'A\' or \'B\'.\nInput -> ').lower()
+if user_answer != 'a' and user_answer != 'b':
+    print('Your input is invalid.')
+    exit()
 
-# if right => You're right! Current score: {score}.
-# if wrong => Sorry, that's wrong. Final score: {score}.
+if data[first_artist]['followers'] > data[second_artist]['followers']:
+    if user_answer == 'a':
+        score += 1
+        print(f'You\'re right! Current score: {score}.')
+    else:
+        os.system('cls')
+        print(art.logo)
+        print(f'Sorry, that\'s wrong. Final score: {score}.\n')
+else:
+    if user_answer == 'b':
+        score += 1
+        print(f'You\'re right! Current score: {score}.')
+    else:
+        os.system('cls')
+        print(art.logo)
+        print(f'Sorry, that\'s wrong. Final score: {score}.\n')
