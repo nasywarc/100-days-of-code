eb75e47b1492aad5b1ebb8548c5afec2e29d99ba
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-27 15:45:26 +0700

    making the func and the stock

---
+++ day-15/main_testing.py
@@ -0,0 +1,21 @@
+import os
+import random
+from coffee_machine import MENU
+from coffee_machine import resources
+
+
+def ingredients():
+    pass
+
+
+def calc():
+    pass
+
+
+def report():
+    pass
+
+
+water_stock = ingredients['water']
+milk_stock = ingredients['milk']
+coffee_stock = ingredients['coffee']
