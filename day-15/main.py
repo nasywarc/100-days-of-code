cff866090bf447a809bf3a8efa7dff9a1c9b9800
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-29 19:47:12 +0700

    deleting the main, renaming the main_testing to main.py

--- day-15/main.py
+++ day-15/main.py
@@ -1,72 +1,107 @@
 import os
-import random
 from coffee_machine import MENU
 from coffee_machine import resources
 
 
-def water():
-    remain_water = int()
-    if user_choice == 'espresso':
-        remain_water = resources['water'] - \
-            MENU['espresso']['ingredients']['water']
-    elif user_choice == 'latte':
-        remain_water = resources['water'] - \
-            MENU['latte']['ingredients']['water']
-    elif user_choice == 'cappuccino':
-        remain_water = resources['water'] - \
-            MENU['cappuccino']['ingredients']['water']
-    return remain_water
+def insert_coin(price, coffee, emote):
+    print('Please insert coins.')
+    quarters = float(input('Quarters: '))
+    dimes = float(input('Dimes\t: '))
+    nickles = float(input('Nickles\t: '))
+    pennies = float(input('Pennies\t: '))
+    total = (0.25 * quarters) + (0.1 * dimes) + \
+        (0.05 * nickles) + (0.01 * pennies)
+    if total > price:
+        print(f'\nHere is ${round(total-price, 2)} in change.')
+        print(f'\nHere is your {coffee} {emote}\nEnjoy!\n')
+        return True
+    elif total < price:
+        print('\nSorry that\'s not enough money. Money refunded.\n')
+        return False
+    else:
+        print(f'\nHere is your {coffee} {emote}\nEnjoy!\n')
+        return True
 
 
-def milk():
-    remain_milk = int()
-    if user_choice == 'espresso':
-        remain_milk = resources['milk']
-    elif user_choice == 'latte':
-        remain_milk = resources['milk'] - MENU['latte']['ingredients']['milk']
-    elif user_choice == 'cappuccino':
-        remain_milk = resources['milk'] - \
-            MENU['cappuccino']['ingredients']['milk']
-    return remain_milk
-
-
-def coffee():
-    remain_coffee = int()
-    if user_choice == 'espresso':
-        remain_coffee = resources['coffee'] - \
-            MENU['espresso']['ingredients']['coffee']
-    elif user_choice == 'latte':
-        remain_coffee = resources['coffee'] - \
-            MENU['latte']['ingredients']['coffee']
-    elif user_choice == 'cappuccino':
-        remain_coffee = resources['coffee'] - \
-            MENU['cappuccino']['ingredients']['coffee']
-    return remain_coffee
+def money_count(coffee):
+    global money_earned
+    money_earned += MENU[f'{coffee}']['cost']
 
 
 def report():
-    print(f'\nWater : {resources["water"]}ml')
-    print(f'Milk : {resources["milk"]}ml')
-    print(f'Coffee : {resources["coffee"]}ml')
+    print(f'Water\t: {water_stock}ml')
+    print(f'Milk\t: {milk_stock}ml')
+    print(f'Coffee\t: {coffee_stock}g')
+    print(f'Money\t: ${money_earned}\n')
 
 
-def print_all():
-    print(water())
-    print(milk())
-    print(coffee())
+def checking_stock(coffee):
+    if water_stock - MENU[f'{coffee}']['ingredients']['water'] >= 0:
+        if coffee != 'espresso':
+            if milk_stock - MENU[f'{coffee}']['ingredients']['milk'] >= 0:
+                if coffee_stock - MENU[f'{coffee}']['ingredients']['coffee'] >= 0:
+                    return 'ready'
+                else:
+                    return 'coffee'
+            else:
+                return 'milk'
+        else:
+            if coffee_stock - MENU[f'{coffee}']['ingredients']['coffee'] >= 0:
+                return 'ready'
+    else:
+        return 'water'
 
 
+def charge_stock(coffee):
+    global water_stock, milk_stock, coffee_stock
+    check_stock = True
+
+    water_stock -= MENU[f'{coffee}']['ingredients']['water']
+    if coffee != 'espresso' and check_stock:
+        milk_stock -= MENU[f'{coffee}']['ingredients']['milk']
+    if check_stock:
+        coffee_stock -= MENU[f'{coffee}']['ingredients']['coffee']
+
+    return check_stock
+
+
+def step(choice):
+    if checking_stock(choice) == 'ready':
+        if insert_coin(price, user_choice, emote):
+            charge_stock(choice)
+            money_count(choice)
+    else:
+        print(f'Sorry there is not enough {checking_stock(choice)}.\n')
+
+
+money_earned = 0
+program = True
+water_stock = resources['water']
+milk_stock = resources['milk']
+coffee_stock = resources['coffee']
 os.system('cls')
-user_choice = input(
-    'What would you like? (espresso/latte/cappuccino)\nInput -> ')
-if user_choice == 'espresso':
-    print_all()
-elif user_choice == 'latte':
-    print_all()
-elif user_choice == 'cappuccino':
-    print_all()
-elif user_choice == 'off':
-    print('You\'ve turned off the coffee machine.')
-    exit()
-elif user_choice == 'report':
-    report()
+
+while program:
+    user_choice = input(
+        'What would you like? (espresso/latte/cappuccino) : ')
+
+    if user_choice == 'espresso':
+        emote = '☕'
+        price = MENU['espresso']['cost']
+        step(user_choice)
+    elif user_choice == 'latte':
+        emote = '🥤'
+        price = MENU['latte']['cost']
+        step(user_choice)
+    elif user_choice == 'cappuccino':
+        emote = '🍵'
+        price = MENU['cappuccino']['cost']
+        step(user_choice)
+    elif user_choice == 'off':
+        print('\nYou\'ve turned off the coffee machine.')
+        program = False
+    elif user_choice == 'report':
+        report()
+    else:
+        print('\nYour input is invalid.')
+        program = False
