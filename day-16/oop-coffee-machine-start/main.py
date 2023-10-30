37a40a2107ec4a9f501026f4d46fcfe21652b41d
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-30 20:01:53 +0700

    trial n error 2

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -2,11 +2,37 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-program = True
-latte = MenuItem('latte', 200, 150, 24, 2.5)
-# latte = MenuItem(name='latte', water=200, milk=150, coffee=24, cost=2.5)
 
-while program:
-    user_choice = str(input('Espresso, latte, cappuccino? '))
-    if user_choice == latte.name:
-        print(f'{latte.name}\n{latte.cost}')
+def latte():
+    global name, cost, ingredients
+    name = 'latte'
+    cost = 2.5
+    ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
+
+
+def latte():
+    global name, cost, ingredients
+    name = 'latte'
+    cost = 2.5
+    ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
+
+
+def latte():
+    global name, cost, ingredients
+    name = 'latte'
+    cost = 2.5
+    ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
+
+
+name = ''
+cost = int()
+ingredients = {}
+
+# TODO : 1. Ask user what they want
+user_input = input('Choice : ')
+# TODO : 2. Check resources for the order
+
+# TODO : 3. If resources available, ask the user for their money
+# TODO : 4. If not print
+# TODO : 5. If their money enough, minus the resources
+# TODO : 6. If success, print to user
