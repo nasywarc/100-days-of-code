71b717a64dc9ef132e4a6b06f3f6fe0fb231184b
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-30 19:48:08 +0700

    trial n error, pycache

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -1,3 +1,12 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+program = True
+latte = MenuItem('latte', 200, 150, 24, 2.5)
+# latte = MenuItem(name='latte', water=200, milk=150, coffee=24, cost=2.5)
+
+while program:
+    user_choice = str(input('Espresso, latte, cappuccino? '))
+    if user_choice == latte.name:
+        print(f'{latte.name}\n{latte.cost}')
