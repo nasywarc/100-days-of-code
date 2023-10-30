8c2ecc47eb9acb212d94185aadb04ca28ea6a599
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-30 20:22:31 +0700

    commenting all the code

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -3,38 +3,49 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-def espresso():
-    global name, cost, ingredients
-    name = 'latte'
-    cost = 2.5
-    ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
-
-
-def latte():
-    global name, cost, ingredients
-    name = 'latte'
-    cost = 2.5
-    ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
-
-
-def cappuccino():
-    global name, cost, ingredients
-    name = 'latte'
-    cost = 2.5
-    ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
-
-
-name = ''
-cost = int()
-ingredients = {}
-
 # TODO : 1. Ask user what they want
-user_input = input('Choice : ')
 # TODO : 2. Check resources for the order
-(f'{user_input}()')
-user_input_instance = CoffeeMaker()
-user_input_instance.is_resource_sufficient(drink=user_input)
 # TODO : 3. If resources available, ask the user for their money
 # TODO : 4. If not print
 # TODO : 5. If their money enough, minus the resources
 # TODO : 6. If success, print to user
+
+
+# def user_coffee(choice):
+#     global name, cost, ingredients
+
+#     if choice == 'latte':
+#         name = 'latte'
+#         cost = 2.5
+#         ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
+#     elif choice == 'espresso':
+#         name = 'espresso'
+#         cost = 2.5
+#         ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
+#     elif choice == 'cappuccino':
+#         name = 'cappuccino'
+#         cost = 2.5
+#         ingredients = {'water': 100, 'milk': 150, 'coffee': 24}
+
+
+# def espresso():
+#     global name, cost, ingredients
+
+
+# def latte():
+#     global name, cost, ingredients
+
+
+# def cappuccino():
+#     global name, cost, ingredients
+
+
+# name = ''
+# cost = int()
+# ingredients = {}
+
+# user_input = input('Choice : ')
+# user_coffee(user_input)
+
+# user_input_instance = CoffeeMaker()
+# user_input_instance.is_resource_sufficient(drink=user_input)
