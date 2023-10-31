bb35fa4edef08f00943bd0dc1a22a7c236e05fea
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-31 11:07:08 +0700

    deleting all the codes 2

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -1,15 +1,3 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
-# MenuItem()
-# Menu()
-# CoffeeMaker()
-# MoneyMachine()
-
-user_input = input("Choice : ")
-
-if user_input == 'latte':
-    print(user_input.menu([2]))
-
-user_input = Menu()
