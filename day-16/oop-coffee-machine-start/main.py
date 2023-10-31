7df974237d86e82f5fb4c61fda81fc702ebdf442
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-31 11:10:10 +0700

    still error

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -1,3 +1,9 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee = Menu()
+
+user_choice = input('Choice : ')
+
+for coffee.menu() 
