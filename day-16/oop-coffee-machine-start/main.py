465bd1f954c699d3623ba8b940888e2d4f9774d4
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-05 21:29:08 +0700

    coffee maker class

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -2,9 +2,9 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-drinks = Menu()
+menu_item = Menu()
+coffee_maker = CoffeeMaker()
 
 user_input = input('Choice : ')
 
-drinks.find_drink(user_input)
-drinks.get_items()
+coffee_maker.is_resource_sufficient(user_input)
