16eb35510120101713f02b18711cb9ff097acd20
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-05 10:30:07 +0700

    deleting all the code except the module

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -1,10 +1,3 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
-item = Menu()
-
-user_input = input('Choice : ')
-
-item.find_drink(user_input)
-item.get_items()
