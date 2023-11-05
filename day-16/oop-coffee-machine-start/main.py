1fe8222337e2fa4b71302cc99f945f1affa0d1ef
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-05 21:25:54 +0700

    trial n error for .. times

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -1,3 +1,10 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+drinks = Menu()
+
+user_input = input('Choice : ')
+
+drinks.find_drink(user_input)
+drinks.get_items()
