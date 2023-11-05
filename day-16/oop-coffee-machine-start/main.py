f0c718b5789d8ac9b22d500ef794c1ab5e50b589
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-05 21:35:55 +0700

    deleting codes

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -5,9 +5,3 @@ from money_machine import MoneyMachine
 menu_item = Menu()
 order_name = Menu()
 coffee_maker = CoffeeMaker()
-
-order_name = input('Choice : ')
-if order_name.get_items():
-    print('menu available')
-else:
-    print('not available')
