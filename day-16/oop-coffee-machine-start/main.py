7c552145f266c06282ee7938f70131810365b8d7
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-05 21:39:15 +0700

    deleting all the codes again

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -1,14 +1,3 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
-menu_item = Menu()
-order_name = Menu()
-coffee_maker = CoffeeMaker()
-
-choice = 'latte'
-
-if order_name.find_drink(choice) == True:
-    print('right')
-else:
-    print('wrong')
