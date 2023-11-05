19546ac546daaafdbab3a5e0fdb55717a71a9451
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-05 21:34:47 +0700

    trial n error

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -3,8 +3,11 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu_item = Menu()
+order_name = Menu()
 coffee_maker = CoffeeMaker()
 
-user_input = input('Choice : ')
-
-coffee_maker.is_resource_sufficient(user_input)
+order_name = input('Choice : ')
+if order_name.get_items():
+    print('menu available')
+else:
+    print('not available')
