4813ae39d2d2ef19729f6f5ed8a4ddaef9a86187
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-06 12:54:43 +0700

    clear terminal

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -1,12 +1,14 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
 
 menu_object = Menu()
 coffee_object = CoffeeMaker()
 money_object = MoneyMachine()
 off = False
 
+os.system('cls')
 while not off:
     user_input = input(f'What would you like? {menu_object.get_items()} : ')
     if user_input == 'off':
