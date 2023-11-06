e38f3a7b8679fdc4e25223b86d3d0180999f22a0
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-06 12:52:24 +0700

    finally done(??)

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -5,15 +5,18 @@ from money_machine import MoneyMachine
 menu_object = Menu()
 coffee_object = CoffeeMaker()
 money_object = MoneyMachine()
+off = False
 
-user_input = input(f'What would you like? {menu_object.get_items()} : ')
-if user_input == 'off':
-    print('off')
-elif user_input == 'report':
-    print('report')
-else:
-    find = menu_object.find_drink(user_input)
-    if find:
-        print('yes')
+while not off:
+    user_input = input(f'What would you like? {menu_object.get_items()} : ')
+    if user_input == 'off':
+        off = True
+    elif user_input == 'report':
+        coffee_object.report()
+        money_object.report()
     else:
-        print('no')
+        find = menu_object.find_drink(user_input)
+        if find:
+            if (coffee_object.is_resource_sufficient(find)):
+                if money_object.make_payment(find.cost):
+                    coffee_object.make_coffee(find)
