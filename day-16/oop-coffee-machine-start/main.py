5776edd9d15d0397a09b9780ec85e16deff8c494
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-06 12:43:14 +0700

    writing code

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -5,3 +5,15 @@ from money_machine import MoneyMachine
 menu_object = Menu()
 coffee_object = CoffeeMaker()
 money_object = MoneyMachine()
+
+user_input = input(f'What would you like? {menu_object.get_items()} : ')
+if user_input == 'off':
+    print('off')
+elif user_input == 'report':
+    print('report')
+else:
+    find = menu_object.find_drink(user_input)
+    if find:
+        print('yes')
+    else:
+        print('no')
