4ddf9baf882e8c4302461148f0627135e62e3fa3
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-06 17:20:03 +0700

    simplify

--- day-16/oop-coffee-machine-start/main.py
+++ day-16/oop-coffee-machine-start/main.py
@@ -10,7 +10,7 @@ off = False
 
 os.system('cls')
 while not off:
-    user_input = input(f'What would you like? {menu_object.get_items()} : ')
+    user_input = input(f'What would you like? ({menu_object.get_items()}) : ')
     if user_input == 'off':
         off = True
     elif user_input == 'report':
@@ -19,6 +19,5 @@ while not off:
     else:
         find = menu_object.find_drink(user_input)
         if find:
-            if (coffee_object.is_resource_sufficient(find)):
-                if money_object.make_payment(find.cost):
-                    coffee_object.make_coffee(find)
+            if (coffee_object.is_resource_sufficient(find)) and money_object.make_payment(find.cost):
+                coffee_object.make_coffee(find)
