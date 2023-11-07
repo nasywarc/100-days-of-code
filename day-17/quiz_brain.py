6ed9bfe7266f78890ec99fcbbdc125138a0fc672
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-07 11:52:14 +0700

    modify code

--- day-17/quiz_brain.py
+++ day-17/quiz_brain.py
@@ -1,3 +1,4 @@
 class QuizBrain:
-    def __init__(self, question_number, question_list):
-        question_number = 0
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
