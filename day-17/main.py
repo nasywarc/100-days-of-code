867208e529a7e251c465af3fef76532e8aca587e
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-08 07:24:22 +0700

    modify main code

--- day-17/main.py
+++ day-17/main.py
@@ -8,6 +8,6 @@ for index in range(len(question_data)-1):
     question_bank.append(
         Question(question_data[index]['text'], question_data[index]['answer']))
 
-question = QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
 
-question.next_question()
+quiz.next_question()
