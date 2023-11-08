35c01ef7292ce0cdab2beefe5fd39a5f920d6c75
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-08 23:49:10 +0700

    string method

--- day-17/youtube tutorial/oop_for_beginners.py
+++ day-17/youtube tutorial/oop_for_beginners.py
@@ -1,9 +1,15 @@
-# Object Oriented Programming in Python
+# # Object Oriented Programming in Python
 
-def hello():
-    print('hello')
+# def hello():
+#     print('hello')
 
 
-x = 1
-print(type(hello))
-print(type(x))
+# x = 1
+
+# print(type(hello))
+# print(type(x))
+
+
+string = 'hello'
+print(string.upper())
+# # string method
