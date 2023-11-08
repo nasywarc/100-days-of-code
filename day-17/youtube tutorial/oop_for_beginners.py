59529eab82b152b4ca9c0824ab3396a66554fabc
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-09 00:21:58 +0700

    creating objects

--- day-17/youtube tutorial/oop_for_beginners.py
+++ day-17/youtube tutorial/oop_for_beginners.py
@@ -54,5 +54,25 @@ class Student:
 class Course:
     def __init__(self, name, max_student):
         self.name = name
-        self.max_student = max_student
-        self.student = []
+        self.max_students = max_student
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += Student.get_grade()
+
+
+s1 = Student('Tim', 19, 95)
+s2 = Student('Bill', 19, 75)
+s3 = Student('Jill', 19, 65)
+
+course = Course('Science', 2)
+course.add_student(s1)
+course.add_student(s2)
