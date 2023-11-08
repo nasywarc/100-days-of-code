d3394d39050374f7f95db876a6c538f6222fbec5
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-09 00:02:12 +0700

    get name get age metho

--- day-17/youtube tutorial/oop_for_beginners.py
+++ day-17/youtube tutorial/oop_for_beginners.py
@@ -16,12 +16,21 @@
 
 class Dog:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, age):
         # make attribute
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
 
 
-d = Dog('Tim')
-print(d.name)
-d2 = Dog('Bill')
-print(d2.name)
+d = Dog('Tim', 34)
+print(d.get_name())
+print(d.get_age())
+d2 = Dog('Bill', 12)
+print(d2.get_name())
+print(d2.get_age())
