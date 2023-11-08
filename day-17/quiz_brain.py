e5b63ce0324967d6b997470a04a4dca9c1384f29
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-08 07:23:06 +0700

    next_question func

--- day-17/quiz_brain.py
+++ day-17/quiz_brain.py
@@ -4,5 +4,5 @@ class QuizBrain:
         self.question_list = question_list
 
     def next_question(self):
-        answer = input(
-            f'Q.{(self.question_number)+1}{self.question_list[self.question_number]}')
+        current_question = self.question_list[self.question_number]
+        input(f'Q.{self.question_number}: {current_question.text} (True/False)?:')
