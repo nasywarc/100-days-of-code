9907037ca1bbd0aebb1a5f058b0e5a9bf10cb0f9
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-11 15:15:44 +0700

    making dOG class

--- day-17/youtube tutorial/inheritance.py
+++ day-17/youtube tutorial/inheritance.py
@@ -5,3 +5,12 @@ class Cat:
 
     def speak(self):
         print('Meow')
+
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print('Bark')
