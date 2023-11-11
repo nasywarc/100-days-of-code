4a6448826a4dcb741bcbdfd41a42e22800e2314a
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-11 15:22:00 +0700

    same code but with inheritance

--- day-17/youtube tutorial/inheritance.py
+++ day-17/youtube tutorial/inheritance.py
@@ -1,17 +1,44 @@
-class Cat:
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def speak(self):
+#         print('Meow')
+
+
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def speak(self):
+#         print('Bark')
+
+
+# c1 = Cat('Kitty', 1)
+# d1 = Dog('Doggy', 2)
+
+# c1.speak()
+# d1.speak()
+
+
+# with inheritance
+class Animal:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def speak(self):
+        pass
+
+
+class Cat(Animal):
+    def speak(self):
         print('Meow')
 
 
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
+class Dog(Animal):
     def speak(self):
         print('Bark')
 
