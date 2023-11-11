2901d91cd2f2a111d898306a5297c0e29b0887fb
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-11 16:02:14 +0700

    class method implementation

--- day-17/youtube tutorial/class_attribute.py
+++ day-17/youtube tutorial/class_attribute.py
@@ -3,10 +3,18 @@ class Person:
 
     def __init__(self, name):
         self.name = name
-        Person.number_of_people += 1
+        Person.add_person()
+
+    @classmethod
+    def number_of_people_(cls):
+        return cls.number_of_people
+
+    @classmethod
+    def add_person(cls):
+        cls.number_of_people += 1
 
 
 p1 = Person('Tim')
-print(Person.number_of_people)
+print(Person.number_of_people_())
 p2 = Person('Jack')
-print(Person.number_of_people)
+print(Person.number_of_people_())
