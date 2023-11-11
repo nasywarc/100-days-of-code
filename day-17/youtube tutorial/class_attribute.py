8fe78cf6143bc01e42cfd836c6250612ee298537
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-11 16:11:45 +0700

    staticmethod implementation

--- day-17/youtube tutorial/class_attribute.py
+++ day-17/youtube tutorial/class_attribute.py
@@ -1,20 +1,39 @@
-class Person:
-    number_of_people = 0
+# # classmethod implementation
+# class Person:
+#     number_of_people = 0
 
-    def __init__(self, name):
-        self.name = name
-        Person.add_person()
+#     def __init__(self, name):
+#         self.name = name
+#         Person.add_person()
 
-    @classmethod
-    def number_of_people_(cls):
-        return cls.number_of_people
+#     @classmethod
+#     def number_of_people_(cls):
+#         return cls.number_of_people
 
-    @classmethod
-    def add_person(cls):
-        cls.number_of_people += 1
+#     @classmethod
+#     def add_person(cls):
+#         cls.number_of_people += 1
 
 
-p1 = Person('Tim')
-print(Person.number_of_people_())
-p2 = Person('Jack')
-print(Person.number_of_people_())
+# p1 = Person('Tim')
+# print(Person.number_of_people_())
+# p2 = Person('Jack')
+# print(Person.number_of_people_())
+
+
+# staticmethod implementation
+class Math:
+    @staticmethod
+    def add5(x):
+        return x + 5
+
+    def add10(x):
+        return x + 10
+
+    def pr():
+        print('run')
+
+
+print(Math.add5(5))
+print(Math.add10(5))
+Math.pr()
