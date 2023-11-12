2ea4fcfc136fd4e9f73e9cdb8c3e51b51d330590
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-12 21:28:08 +0700

    add method

--- day-17/youtube tutorial/oop_implementation.py
+++ day-17/youtube tutorial/oop_implementation.py
@@ -1,5 +1,11 @@
 class Person:
+    person_count = 0
+
     def __init__(self, name, age, address):
         self.name = name
         self.age = age
         self.address = address
+        Person.add()
+
+    def add(cls):
+        Person.person_count += 1
