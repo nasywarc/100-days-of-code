73c0cd8b68ffc3ac07d50497750581d01c6fcf32
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-15 12:36:47 +0700

    showing score to user

--- day-17/quiz_brain.py
+++ day-17/quiz_brain.py
@@ -21,4 +21,5 @@ class QuizBrain:
         else:
             print('That\'s wrong.')
         print(f'The correct answer is {correct_answer}.')
-        print(f'Score = {self.score}', end='\n\n')
+        print(
+            f'The current score is {self.score}/{self.question_number}', end='\n\n\n')
