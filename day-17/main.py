fbcec0c7a887befa76a1748d97bbba9978fe9809
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-15 12:13:51 +0700

    deleting space

--- day-17/main.py
+++ day-17/main.py
@@ -3,15 +3,11 @@ from question_model import Question
 from quiz_brain import QuizBrain
 
 question_bank = []
-
 for index in range(len(question_data)):
     question_bank.append(
         Question(question_data[index]['text'], question_data[index]['answer']))
 
 quiz = QuizBrain(question_bank)
-
-# for question in range(len(question_bank)):
-#     quiz.next_question()
 
 while quiz.still_has_question():
     quiz.next_question()
