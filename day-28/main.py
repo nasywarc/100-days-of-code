737b87e49293786a2451edb997a20238fce30e68
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-16 12:47:27 +0700

    completing the app UI

--- day-28/main.py
+++ day-28/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,10 +9,32 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato)
+canvas.create_text(100, 130, text='00:00', fill='white',
+                   font=(FONT_NAME, 35, 'bold'))
+canvas.grid(row=1, column=1)
+
+
+timer_label = Label(text='Timer', fg=GREEN, font=(FONT_NAME, 30), bg=YELLOW)
+timer_label.grid(row=0, column=1)
+
+start_button = Button(text='Start')
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text='Reset')
+reset_button.grid(row=2, column=2)
+
+window.mainloop()
