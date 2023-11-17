db12ad2a850851d4e62c257172d79b42881ab094
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-17 17:14:49 +0700

    countdown and timer mechanism, startbutton command

--- day-28/main.py
+++ day-28/main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,7 +14,19 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    count_down(5)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,18 +36,21 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato)
-canvas.create_text(100, 130, text='00:00', fill='white',
-                   font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white',
+                                font=(FONT_NAME, 35, 'bold'))
 canvas.grid(row=1, column=1)
 
 
-timer_label = Label(text='Timer', fg=GREEN, font=(FONT_NAME, 30), bg=YELLOW)
+timer_label = Label(text='Timer', fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
 timer_label.grid(row=0, column=1)
 
-start_button = Button(text='Start')
+checkmark_label = Label(text='✔️', fg=GREEN, bg=YELLOW)
+checkmark_label.grid(row=3, column=1)
+
+start_button = Button(text='Start', highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text='Reset')
+reset_button = Button(text='Reset', highlightthickness=0)
 reset_button.grid(row=2, column=2)
 
 window.mainloop()
