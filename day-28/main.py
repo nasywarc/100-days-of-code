ef9da27652efc2b6a3d61e9eb3be9c275d3dd976
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-17 17:22:22 +0700

    minutes and second timer

--- day-28/main.py
+++ day-28/main.py
@@ -1,4 +1,5 @@
 import time
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,14 +17,18 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5)
+    count_down(5*60)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
         window.after(1000, count_down, count - 1)
 
