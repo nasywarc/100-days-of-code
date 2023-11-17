3628c9ae476c79098b21b59be06ca0016d87d7d8
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-17 17:45:45 +0700

    start_timer logic

--- day-28/main.py
+++ day-28/main.py
@@ -10,6 +10,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
@@ -17,8 +18,19 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5*60)
+    global reps
+    reps += 1
 
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
