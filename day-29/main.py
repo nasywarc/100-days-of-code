8496def541ae519c813a4f07b860343b376791d5
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-18 08:44:54 +0700

    logo image positioning

--- day-29/main.py
+++ day-29/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=220, height=220, highlightthickness=0)
+logo = PhotoImage(file='logo.png')
+canvas.create_image(110, 110, image=logo)
+canvas.pack()
+
+
+window.mainloop()
