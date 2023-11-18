277a931c972ad6cc439daf0f7480850023afb676
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-18 10:26:05 +0700

    creating data.txt for saving password, creating save_password func

--- day-29/main.py
+++ day-29/main.py
@@ -1,7 +1,22 @@
 from tkinter import *
+import os
+
+file_path = 'data.txt'
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save_password():
+    if os.path.exists(file_path):
+        with open(file_path, "a", newline='', encoding="cp437", errors='ignore') as pass_file:
+            pass_file.write(
+                f'\n{website_entry.get()}\t\t{email_usn_entry.get()}\t\t{password_entry.get()}')
+    else:
+        with open(file_path, "x", newline='', encoding="cp437", errors='ignore') as pass_file:
+            pass_file.write(
+                f'{website_entry.get()}\t\t{email_usn_entry.get()}\t\t{password_entry.get()}')
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,9 +39,11 @@ password_label.grid(row=3, column=0)
 
 website_entry = Entry()
 website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.focus()  # auto focusing the cursor
 
 email_usn_entry = Entry()
 email_usn_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
+email_usn_entry.insert(0, 'dummy@gmail.com')
 
 password_entry = Entry()
 password_entry.grid(row=3, column=1, sticky="EW")
@@ -34,7 +51,7 @@ password_entry.grid(row=3, column=1, sticky="EW")
 generate_button = Button(text='Generate Password')
 generate_button.grid(row=3, column=2, sticky="EW")
 
-add_button = Button(text='Add')
+add_button = Button(text='Add', command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
