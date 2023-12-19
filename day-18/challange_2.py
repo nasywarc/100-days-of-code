04f5c26d4cd54fdb0f598f3e4750e4f4626bea25
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-19 15:19:54 +0700

    dashed line challage

---
+++ day-18/challange_2.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 2 - Draw a Dashed Line ########
+
+for times in range(25):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+    
+t.mainloop()
