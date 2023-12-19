6a4a54ea793b99ef1afb709f92621720b8eaa772
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-19 12:24:12 +0700

    reducing code with a loop

--- day-18/main.py
+++ day-18/main.py
@@ -4,14 +4,9 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape('turtle')
 timmy_the_turtle.color('red')
 
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-
+for times in range(0, 4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
 
 screen = Screen()
 screen.exitonclick()
