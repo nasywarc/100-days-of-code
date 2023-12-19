04fcdcfc21c87d475060acf0ff6e5d32925387ad
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-19 15:38:41 +0700

    finish from triangle to decagon

--- day-18/challange_3.py
+++ day-18/challange_3.py
@@ -7,5 +7,33 @@ tim = t.Turtle()
 for triangle in range(3):
     tim.forward(100)
     tim.right(120)
+    
+for square in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+for pentagon in range(5):
+    tim.forward(100)
+    tim.right(72)
+
+for hexagon in range(6):
+    tim.forward(100)
+    tim.right(60)
+
+for heptagon in range(7):
+    tim.forward(100)
+    tim.right(51.428)
+
+for octagon in range(8):
+    tim.forward(100)
+    tim.right(45)
+
+for nonagon in range(9):
+    tim.forward(100)
+    tim.right(40)
+
+for decagon in range(10):
+    tim.forward(100)
+    tim.right(36)
 
 t.mainloop()
