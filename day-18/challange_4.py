02636a89f071eb6f58279a89cf5d231daede1286
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-20 08:46:53 +0700

    random color with rgb

--- day-18/challange_4.py
+++ day-18/challange_4.py
@@ -2,16 +2,21 @@ import turtle as t
 import random
 
 tim = t.Turtle()
+t.colormode(255)
 
 ########### Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
 tim.pensize(10)
 tim.speed('fastest')
 
 for times in range(200):
     direction = random.choice([1, 2, 3, 4])
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.right(direction * 90)
     tim.forward(20)
     
