c45060b71c3671c3ab4710844e4025e036f097fd
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-25 08:49:14 +0700

    etch a sketch finish

--- day-19/main.py
+++ day-19/main.py
@@ -7,8 +7,20 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
     
+def move_backwards():
+    tim.back(10)
+    
+def clockwise():
+    tim.right(10)
+    
+def c_clockwise():
+    tim.left(10)
+    
 screen.listen()
-screen.onkey(key='space', fun=move_forwards)
+screen.onkey(key='w', fun=move_forwards)
+screen.onkey(key='a', fun=c_clockwise)
+screen.onkey(key='s', fun=move_backwards)
+screen.onkey(key='d', fun=clockwise)
 
 
 screen.exitonclick()
