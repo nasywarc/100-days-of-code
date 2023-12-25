4db0903099240fb51f34e11279da63cb2aaafe19
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-25 18:55:24 +0700

    change the y index list

--- day-19/final_challange.py
+++ day-19/final_challange.py
@@ -6,7 +6,7 @@ window = Screen()
 window.setup(width=500, height=400)
 user_bet = window.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_positions = [-70, -40, -10, 20, 50, 80]
+y_index = [-125, -75, -25, 25, 75, 125]
 all_turtles = []
 
 #Create 6 turtles
@@ -14,7 +14,7 @@ for turtle_index in range(0, 6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
     new_turtle.color(colors[turtle_index])
-    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle.goto(x=-230, y=y_index[turtle_index])
     all_turtles.append(new_turtle)
 
 if user_bet:
