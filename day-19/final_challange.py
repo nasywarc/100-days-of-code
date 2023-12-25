eaea540e87994f0d1213148fcb9f2120017b5905
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-25 18:54:25 +0700

    rename symbol

--- day-19/final_challange.py
+++ day-19/final_challange.py
@@ -2,9 +2,9 @@ from turtle import Turtle, Screen
 import random
 
 is_race_on = False
-screen = Screen()
-screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+window = Screen()
+window.setup(width=500, height=400)
+user_bet = window.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
 all_turtles = []
@@ -35,4 +35,4 @@ while is_race_on:
         rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
 
-screen.exitonclick()
+window.exitonclick()
