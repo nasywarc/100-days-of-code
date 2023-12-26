e52c0d8c92ec6a6b09f6fb98142dfa72dff51100
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-26 21:40:05 +0700

    deleting some code

--- day-20/main.py
+++ day-20/main.py
@@ -4,16 +4,13 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
-snake_length = []
 x_index = [-50, -30, -10]
 
 for square in range(0, 3):
     new_square = Turtle(shape='square')
     new_square.penup()
     new_square.color('white')
-    new_square.goto(x=x_index[square], y=-10)
-    snake_length.append(new_square)
-    
+    new_square.goto(x=x_index[square], y=-10)    
     
 
 
