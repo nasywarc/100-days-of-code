2c2aad35034603e7c51c058d6cde355a821c37da
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-26 21:30:37 +0700

    trial error 1

--- day-20/main.py
+++ day-20/main.py
@@ -4,6 +4,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+snake_length = []
+x_index = [-2, -1, 0]
+
+for square in range(0, 3):
+    new_square = Turtle(shape='square')
+    new_square.color('white')
+    new_square.goto(x=x_index[square], y=0)
+    snake_length.append(new_square)
+    
+    
 
 
 screen.exitonclick()
