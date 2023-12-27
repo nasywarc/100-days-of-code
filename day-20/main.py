0d8a3bf13d1d61c1514a69a386144506301c277d
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-27 18:38:42 +0700

    trial error 1

--- day-20/main.py
+++ day-20/main.py
@@ -22,6 +22,8 @@ while game_is_on:
     time.sleep(0.5)
     for square in snake_length:
         square.forward(40)
+        if square.position(x=580) or square.position(x=-580) or square.position(y=580) or square.position(y=-580):
+            game_is_on = False 
 
 
 screen.exitonclick()
