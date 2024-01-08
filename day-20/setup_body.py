241f297be67b9844995c5ed424184cbce2f48463
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-01-08 10:38:51 +0700

    using loop for the segment

--- day-20/setup_body.py
+++ day-20/setup_body.py
@@ -5,8 +5,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 
-segment_1 = Turtle('square')
-segment_1.color('white')
+starting_pos = [(0, 0), (-20, 0), (-40, 0)]
+for position in starting_pos:
+    new_segment = Turtle('square')
+    new_segment.color('white')
+    new_segment.goto(position)
+
+
+
+
+
+
+
 
 
 screen.mainloop()
