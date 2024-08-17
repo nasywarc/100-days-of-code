a7e6786fa4c7c7dd23b1041c207c4cca5e32c27e
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-08-17 19:20:08 +0700

    creating main.py

--- day-20/4_food_generator.py
+++ day-20/4_food_generator.py
@@ -3,24 +3,29 @@ import random
 
 from turtle import Screen, Turtle
 
+
 def w_key():
     if head.heading() == 0 or head.heading() == 180:
         head.setheading(90)
+
 
 def a_key():
     if head.heading() == 90 or head.heading() == 270:
         head.setheading(180)
 
+
 def s_key():
     if head.heading() == 0 or head.heading() == 180:
         head.setheading(270)
+
 
 def d_key():
     if head.heading() == 90 or head.heading() == 270:
         head.setheading(0)
 
+
 screen = Screen()
-screen.tracer(0) # animate is off
+screen.tracer(0)  # animate is off
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
@@ -45,13 +50,13 @@ found_food_pos = False
 while not found_food_pos:
     random_x = random.randint(0, 600)
     random_y = random.randint(0, 600)
-    if random_x % 20 == 0 and random_x >= -300 and random_y >-300 and random_x <= 300 and random_y <=300 and random_y % 20 == 0:
+    if random_x % 20 == 0 and random_x >= -300 and random_y > -300 and random_x <= 300 and random_y <= 300 and random_y % 20 == 0:
         found_food_pos = True
 
 food_pos = (float(random_x), float(random_y))
 food.goto(food_pos)
 food_head = False
-        
+
 game_is_on = True
 while game_is_on:
     screen.update()
@@ -61,21 +66,18 @@ while game_is_on:
     screen.onkey(fun=a_key, key=('a'))
     screen.onkey(fun=s_key, key=('s'))
     screen.onkey(fun=d_key, key=('d'))
-            
+
     for segment in segment_list:
         if segment != head:
             segment.goto(head.position())
             head.forward(20)
-            
+
     if head.position() == food_pos:
         last_pos = segment_list[len(segment_list)-1]
         food.goto(last_pos)
         segment_list.append(food)
 
 print(head.pos)
-
-
-
 
 
 screen.mainloop()
