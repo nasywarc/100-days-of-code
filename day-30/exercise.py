6c63cd2aa98868db6bb54c0804e17b43086b7b52
Nasywa Azizah <nasywaazizah01@gmail.com>
2024-08-18 20:01:49 +0700

    creating exercise.py

---
+++ day-30/exercise.py
@@ -0,0 +1,24 @@
+# fruits = ["Apple", "Pear", "Orange"]
+
+# # Catch the exception and make sure the code runs without crashing.
+
+
+# def make_pie(index):
+#     fruit = fruits[index]
+#     print(fruit + " pie")
+
+
+# make_pie(4)
+
+
+fruits = ["Apple", "Pear", "Orange"]
+
+# Catch the exception and make sure the code runs without crashing.
+
+
+def make_pie(index):
+    fruit = fruits[index]
+    print(fruit + " pie")
+
+
+make_pie(4)
